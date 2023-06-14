ae9aa1876227a2931a4cb6599ad070bccf2f2f5a
Wolumo <wistonjosequintanaosorio@hotmail.com>
2023-06-14 12:40:29 -0600

    Month, AgreggationPlan models add app.cofig Created

---
+++ Aggregate Planing/Model/Month.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregate_Planing.Model
+{
+    public class Month
+    {
+        [Key]
+        public int idMonth { get; set; }
+        public string MonthName { get; set; }
+
+    }
+}
