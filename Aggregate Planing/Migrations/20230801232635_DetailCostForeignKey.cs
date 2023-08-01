48946954593c15689c75678e0f905cf9246726bd
Wolumo <wistonjosequintanaosorio@hotmail.com>
2023-08-01 17:42:30 -0600

    Migration DetailCost Foreign Keys Added

---
+++ Aggregate Planing/Migrations/20230801232635_DetailCostForeignKey.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aggregate_Planing.Migrations
+{
+    /// <inheritdoc />
+    public partial class DetailCostForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey("fk_idMonthCost", "AgreggationDetailsCosts", "idMonth", "Months" );
+            migrationBuilder.AddForeignKey("fk_idPlanCost", "AgreggationDetailsCosts", "idPlan", "AgreggationPlans");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey("fk_idMonthCost", "AgreggationDetailsCosts", "idMonth");
+            migrationBuilder.DropForeignKey("fk_idPlanCost", "AgreggationDetailsCosts", "idPlan");
+        }
+    }
+}
